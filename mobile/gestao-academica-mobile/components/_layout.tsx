e53050fc1d376a5a463466da8760d87f2b433c38
EliasBRodrigues <elias.rborges3@gmail.com>
2024-12-31 00:29:00 -0300

    delete: realocation file

--- mobile/gestao-academica-mobile/components/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Slot } from "expo-router";
-import { Drawer } from 'expo-router/drawer';
-import React from "react";
-
-const DrawerLayout = () => {
-    return <Drawer />
-}
-
-export default DrawerLayout;
