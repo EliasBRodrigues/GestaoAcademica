db426dd2bc0eeb3b4b901def1535c81598994476
EliasBRodrigues <elias.rborges3@gmail.com>
2025-01-03 22:33:17 -0300

    update: screen Matricula

--- mobile/gestao-academica-mobile/app/(no-tabs)/(student)/(components)/_registration.tsx
+++ mobile/gestao-academica-mobile/app/(no-tabs)/(student)/(components)/_registration.tsx
@@ -1,23 +1,75 @@
-import React from 'react';
-import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import LinkButton from '@/components/ExternalLink';
 
-
 export default function Registration() {
+  const handleLogin = () => {
+    //Alert.alert('Sucesso', 'Login realizado com sucesso!');
+    //router.push("/(tabs)/(student)/_home")
+  };
+  const [isButtonEnabled, setIsButtonEnabled] = useState(true);
+  const checkButtonState = () => {
+    const randomCondition = Math.random() < 0.5;
+    setIsButtonEnabled(randomCondition);
+  };
+
+  useEffect(() => {
+    checkButtonState();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.link}>
         <LinkButton href="../../../(tabs)/(student)/_home" iconName="chevron-left" size={32} color="white" />
       </View>
-      {/* Componente de Menu Hambúrguer */}
       <View style={styles.firstContainer}>
         <StatusBar barStyle="light-content" backgroundColor="#709BEF" />
         <Text style={styles.text}>Matrícula</Text>
       </View>
-
       <View style={styles.secondContainer}>
-        {/* Primeira linha de botões */}
-
+        <View style={styles.tableRow}>
+          <Text style={styles.mainText}>Fulano de Ciclano Beltrano</Text>
+        </View>
+        <View style={styles.tableRow}>
+          <Text style={styles.mainText2}>Ciclo: 6</Text>
+        </View>
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            <Text style={styles.mainText}>Rendimento no curso</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>PP</Text>
+            <Text style={styles.note}>84,84%</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>PP(Intercâmbio)</Text>
+            <Text style={styles.note}>67.34%</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>PR</Text>
+            <Text style={styles.note}>8.34</Text>
+          </View>
+        </View>
+        <View style={[styles.table, styles.tableMargin]}>
+          <View style={styles.tableRow}>
+            <Text style={styles.mainText}>Prazo de Integralização</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>Cursado</Text>
+            <Text style={styles.note}>4</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>Máximo</Text>
+            <Text style={styles.note}>10</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.subText}>Faltam</Text>
+            <Text style={styles.note}>6</Text>
+          </View>
+        </View>
+        <TouchableOpacity  style={[styles.button, !isButtonEnabled && styles.buttonDisabled]} onPress={isButtonEnabled ? handleLogin : undefined} disabled={!isButtonEnabled} >
+          <Text style={[styles.buttonText, !isButtonEnabled && styles.buttonTextDisabled]}>Matrícula</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -25,59 +77,137 @@ export default function Registration() {
 
 const styles = StyleSheet.create({
   link: {
-    position: 'relative', top: 10,  left: 10, padding: 20,
+    position: 'relative',
+    top: 10,
+    left: 10,
+    padding: 20,
+  },
+  buttonDisabled: {
+    backgroundColor: '#D3D3D3',
+  },
+  buttonTextDisabled: {
+    color: '#A9A9A9',
+  },
+  table: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#709BEF',
+    borderRadius: 10,
+    marginTop: 5,
+    paddingVertical: 10, 
+    paddingHorizontal: 20, 
+  },
+  tableMargin: {
+    marginTop: 20, 
+  },
+  headerText: {
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: '700',
+    color: 'black',
+  },
+  cellText: {
+    flex: 1,
+    textAlign: 'center',
+    color: 'black',
+  },
+  verticalLine: {
+    width: 1,
+    height: '100%',
+    backgroundColor: '#709BEF',
   },
   container: {
-    flex: 0.5,
-    backgroundColor: '#709BEF',
-  },
-  firstContainer: { // BLUE
     flex: 1,
+    backgroundColor: '#709BEF',
+    marginBottom: 30,
+  },
+  firstContainer: {
+    flex: 0.1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#709BEF',
   },
-  secondContainer: { // WHITE
-    flex: 1.6,
-    justifyContent: 'center',
-    alignItems: 'center',
+  secondContainer: {
+    flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 30,
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  logo: {
-    top: 1,
   },
   text: {
     fontSize: 18,
     color: '#FFF',
     fontWeight: '600',
-    bottom: 10
+    bottom: 15,
+  },
+  mainText: {
+    fontSize: 28,
+    color: '#709BEF',
+    fontWeight: '700',
+    textAlign: 'left',
+    width: '100%',
+    marginBottom: 10,
+  },
+  mainText2: {
+    fontSize: 25,
+    color: '#709BEF',
+    fontWeight: '700',
+    textAlign: 'left',
+    width: '100%',
+    marginBottom: 10,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 10,
+  },
+  subText: {
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'left',
+    width: '70%',
+  },
+  note: {
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'right',
+    width: '30%',
   },
   button: {
-    width: 135,
-    height: 150,
+    width: '70%',
+    height: 57,
     backgroundColor: '#709BEF',
     borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 30, 
+    marginBottom: 20, 
   },
   buttonText: {
     color: '#fff',
     fontSize: 18,
     fontWeight: '700',
-    top: 20
+    textAlign: 'center', 
   },
   icon: {
-    color: '#fff'
+    color: '#fff',
   },
   row: {
     bottom: 30,
-    flexDirection: 'row', // Para exibir os botões lado a lado
-    justifyContent: 'space-between', // Espalha as caixas igualmente
-    marginBottom: 20, // Espaçamento entre as linhas
-    width: '80%', // Defina a largura das linhas para que fiquem dentro da tela
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    width: '80%',
+  },
+  scrollContainer: {
+    flex: 1,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
   },
 });
