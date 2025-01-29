06a307b4cd520a6723d95eab68ccbea4e17d9402
EliasBRodrigues <elias.rborges3@gmail.com>
2025-01-28 21:42:57 -0300

    feat: annotation Configuration on main

--- server/src/main/java/com/gestao_academica_backend/server/ServerApplication.java
+++ server/src/main/java/com/gestao_academica_backend/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.gestao_academica_backend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class ServerApplication {
 
 	public static void main(String[] args) {
