b8bf924ce80095ef082773fbb16570996cfe084c
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-01 19:15:45 -0300

    add: interface Attendance

---
+++ mobile/gestao-academica-mobile/services/types/Attendance.ts
@@ -0,0 +1,9 @@
+export interface Attendance {
+    id: number,
+    subjectEnumeration: string,
+    sigla: string,
+    attendanceDate: Date,
+    attendanceStatus: string,
+    attendancePresent: string,
+    attendanceAbsent: string
+}
