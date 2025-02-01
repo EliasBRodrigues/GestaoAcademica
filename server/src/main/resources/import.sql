608c95c2680017ed6e7e26f01dbd5f100e0ab0f4
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-01 20:30:45 -0300

    refactor: query on table classroom & grade

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO teachers (email, password, name, joining_data) VALUES ('professor1@school.com','prof123','Professor A','2018-03-15'),('professor2@school.com','prof456','Professor B','2019-08-20');
-INSERT INTO grade (name_grade, sigla, subject_hours, subject_format_class, horary) VALUES('Modelagem de Banco de Dados', 'MBD', '80h', 'PRESENCIAL', '19:20h - 20:10h'),('Desenvolvimento Web', 'Web', '80h', 'PRESENCIAL', '18:30h - 19:20h'), ('Computacao em Nuvem', 'Nuvem', '80h', 'PRESENCIAL', '20:10h - 21:00h');
-INSERT INTO classroom (classrooms, grade_id) VALUES('DSM_1', 1),('DSM_2', 2)
+INSERT INTO classroom (classrooms) VALUES('DSM_1'),('DSM_2'), ('DSM_3');
+INSERT INTO grade (name_grade, sigla, subject_hours, subject_format_class, horary, classroom_id) VALUES('Modelagem de Banco de Dados', 'MBD', '80h', 'PRESENCIAL', '19:20h - 20:10h', 1),('Desenvolvimento Web', 'Web', '80h', 'PRESENCIAL', '18:30h - 19:20h', 1), ('Computacao em Nuvem', 'Nuvem', '80h', 'PRESENCIAL', '20:10h - 21:00h', 2);
 INSERT INTO students (name, email, ra, academic_year, classroom_id, grade_id) VALUES('Elias','elias@example.com','RA123467890','2024-2025',1,1),('Jane Smith','jane.smith@example.com','RA5678','2024-2025',2,1);
 INSERT INTO notes (student_id,grade_id, subject,atividade_um,atividade_dois,projeto_integrador,avaliacao_integradora,media) VALUES (1,1,'Modelagem',7.5,8.0,9.0,8.5,9), (1,1,'Nuvem',7.5,8.0,9.0,8.5,9), (1,1,'Web',7.5,8.0,9.0,8.5,9), (2,2,'Web',8.0,7.5,9.0,9.5,8);
 INSERT INTO user(id, email, password) VALUES (1, 'user@example.com', 'userPassword123'),(2, 'seconduser@user.com', '123456789');
