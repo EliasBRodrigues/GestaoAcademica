2160fcf07e1a2c39434106e6bbdfb0d17080644a
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-02 20:38:16 -0300

    unnecessary method

--- server/src/main/java/com/gestao_academica_backend/server/repository/StudentRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/StudentRepository.java
@@ -1,13 +1,18 @@
 package com.gestao_academica_backend.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.gestao_academica_backend.server.models.Grade;
 import com.gestao_academica_backend.server.models.Students;
 
 public interface StudentRepository extends JpaRepository<Students,Long> {
-    @Query("SELECT AVG(g.media) FROM GradeNotes g WHERE g.student.id = :studentId")
-    double calculateStudentAverageGrade(@Param("studentId") Long studentId);
+    // @Query("SELECT AVG(g.media) FROM GradeNotes g WHERE g.student.id = :studentId")
+    // double calculateStudentAverageGrade(@Param("studentId") Long studentId);
 
+    // @Query("SELECT s FROM Students s WHERE s.grade_id = :id")
+    // List<Students> findGradeByStudentId(@Param("id") Long id);
 }
