09dc00e2722119762010ed8541dbc991da72cc41
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-02 20:36:39 -0300

    feat: GradeNotes -> Notes

--- server/src/main/java/com/gestao_academica_backend/server/repository/GradeNotesRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/GradeNotesRepository.java
@@ -1,9 +1,14 @@
 package com.gestao_academica_backend.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.gestao_academica_backend.server.models.GradeNotes;
+import com.gestao_academica_backend.server.models.Notes;
 
-public interface GradeNotesRepository extends JpaRepository<GradeNotes, Long>{
-    
+public interface GradeNotesRepository extends JpaRepository<Notes, Long>{
+    @Query("SELECT n FROM Notes n JOIN n.student s WHERE s.id = :id")
+    List<Notes> finGradeNotesByStudentId(@Param("id") Long id);
 }
