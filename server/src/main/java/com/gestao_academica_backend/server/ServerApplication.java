0f9ed1107aabbce3277a891d82c7677d7adcc22e
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-10 18:50:22 -0300

    model mapper on spring-boot-app

--- server/src/main/java/com/gestao_academica_backend/server/ServerApplication.java
+++ server/src/main/java/com/gestao_academica_backend/server/ServerApplication.java
@@ -1,12 +1,19 @@
 package com.gestao_academica_backend.server;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 public class ServerApplication {
+
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
