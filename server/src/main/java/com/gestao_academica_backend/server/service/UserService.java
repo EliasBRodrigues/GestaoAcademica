215d793571bb8a9bcf5e409220cd0f53b8c94c1e
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-10 19:01:06 -0300

    UserService implements

--- server/src/main/java/com/gestao_academica_backend/server/service/UserService.java
+++ server/src/main/java/com/gestao_academica_backend/server/service/UserService.java
@@ -1,4 +1,16 @@
 package com.gestao_academica_backend.server.service;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import com.gestao_academica_backend.server.models.User;
+
+
 public interface UserService {
+    List<User> getAllUsers();
+    Optional<User> getUserByUsername(String username);
+    Boolean hasUserWithUsername(String username);
+    Boolean hasUserWithEmail(String email);
+    User validateAndGetUserByUsername(String username);
 }
---
+++ server/src/main/java/com/gestao_academica_backend/server/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.gestao_academica_backend.server.service.impl;
+
+
+import lombok.RequiredArgsConstructor;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.gestao_academica_backend.server.exception.UserNotFoundException;
+import com.gestao_academica_backend.server.models.User;
+import com.gestao_academica_backend.server.repository.UserRepository;
+import com.gestao_academica_backend.server.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private ModelMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper mapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Boolean hasUserWithUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public Boolean hasUserWithEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User validateAndGetUserByUsername(String username) {
+        return getUserByUsername(username).orElseThrow(() -> new UserNotFoundException(String.format("NOT FOUND WITH NAME: %s", username)));
+    }
+}
