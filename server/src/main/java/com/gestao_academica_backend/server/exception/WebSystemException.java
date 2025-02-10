4f33252687e2060397a717bc03acbc582b08e67a
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-10 18:48:17 -0300

    feat: SystemException runtime

---
+++ server/src/main/java/com/gestao_academica_backend/server/exception/WebSystemException.java
@@ -0,0 +1,29 @@
+package com.gestao_academica_backend.server.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class WebSystemException extends RuntimeException {
+    private HttpStatus status;
+    private String message;
+
+    public WebSystemException(String message) {
+        super(message);
+    }
+
+    public WebSystemException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
