e6d0b7881e79562e222b34f24bc454ca4744a249
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-10 18:54:38 -0300

    feat: repository User

--- server/src/main/java/com/gestao_academica_backend/server/repository/UserRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.gestao_academica_backend.server.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gestao_academica_backend.server.models.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
