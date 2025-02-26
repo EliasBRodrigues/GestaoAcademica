d0a88073bfe101f6143531683a18c31f27d22e17
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-25 23:21:14 -0300

    remove auth layout

--- mobile/gestao-academica-mobile/app/_layout.tsx
+++ mobile/gestao-academica-mobile/app/_layout.tsx
@@ -7,124 +7,126 @@ import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer'
 import { SCREEN } from '@/types/screen';
 import StackNavigation from '@/app/navigation';
 import _forgot from './screens/(tabs)/(login)/_forgot';
-import Login from './screens/auth/login';
-import Home from './screens/(tabs)/(student)/Home';
 import _caution from './screens/(tabs)/(component)/_caution';
 import _logout from './screens/(tabs)/(login)/_logout';
+import Login from './auth/login';
+// import { AuthProvider } from '@/hooks/AuthContext';
 
 const Drawer = createDrawerNavigator();
 export default function Layout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer.Navigator
-        screenOptions={{
-          drawerPosition: "left",
-          drawerType: "front",
-          drawerActiveTintColor: '#709BEF',
-          drawerStyle: {
-            backgroundColor: 'white',
-            width: 300
-          },
-          headerShown: false,
-          headerStyle: {
-            height: 70,
-            backgroundColor: '#709BEF',
-          },
-          drawerInactiveTintColor: '#709BEF',
-          drawerLabelStyle: {
-            color: '#709BEF'
-          },
-        }}
-        drawerContent={
-          (props) => {
-            return (
-              <SafeAreaView>
-                <View style={{
-                  height: 200,
-                  width: "100%",
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: '#021520',
-                  paddingBottom: 12
-                }}>
-                  <Icon name="user" size={28} color="#aaa" style={styles.icon} />
-                  <Text style={{
-                    fontSize: 22,
-                    color: 'white',
-                    fontWeight: 'bold',
-                    marginVertical: 10
-                  }}>User user user</Text>
-                </View>
-                <DrawerItemList {...props} />
-              </SafeAreaView>
-            )
-          }
-        }
-      >
-         <Drawer.Screen
-          name={SCREEN.LOGIN}
-          component={Login}
-          options={{
+    // <AuthProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer.Navigator
+          screenOptions={{
+            drawerPosition: "left",
+            drawerType: "front",
+            drawerActiveTintColor: '#709BEF',
+            drawerStyle: {
+              backgroundColor: 'white',
+              width: 300
+            },
             headerShown: false,
-            drawerLabel: () => null,
-            drawerItemStyle: {display: 'none'}
+            headerStyle: {
+              height: 70,
+              backgroundColor: '#709BEF',
+            },
+            drawerInactiveTintColor: '#709BEF',
+            drawerLabelStyle: {
+              color: '#709BEF'
+            },
           }}
+          drawerContent={
+            (props) => {
+              return (
+                <SafeAreaView>
+                  <View style={{
+                    height: 200,
+                    width: "100%",
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#021520',
+                    paddingBottom: 12
+                  }}>
+                    <Icon name="user" size={28} color="#aaa" style={styles.icon} />
+                    <Text style={{
+                      fontSize: 22,
+                      color: 'white',
+                      fontWeight: 'bold',
+                      marginVertical: 10
+                    }}>User user user</Text>
+                  </View>
+                  <DrawerItemList {...props} />
+                </SafeAreaView>
+              )
+            }
+          }
         >
-        </Drawer.Screen>
-        <Drawer.Screen
-          name={SCREEN.Home}
-          component={StackNavigation}
-          options={{
-            headerShown: true,
-            title: "SIGA",
-            drawerLabel: "Home",
-            drawerIcon: () => (
-              <Icon name="home" size={28} color="#aaa" style={styles.icon} />
-            )
-          }}
-        >
-        </Drawer.Screen>
-        <Drawer.Screen
-          component={_forgot}
-          name={SCREEN.FORGOT}
-          options={{
-            headerShown: true,
-            title: "Forgot",
-            drawerLabel: "Forgot",
-            drawerIcon: () => (
-              <Icon name="gear" size={28} color="#aaa" style={styles.icon} />
-            )
-          }}
-        >
-        </Drawer.Screen>
-        <Drawer.Screen
-          component={_caution}
-          name="(component)/_caution"
-          options={{
-            headerShown: true,
-            title: "Avisos",
-            drawerLabel: "Avisos",
-            drawerIcon: () => (
-              <Icon name="thumb-tack" size={28} color="#aaa" style={styles.icon} />
-            )
-          }}
-        >
-        </Drawer.Screen>
-        <Drawer.Screen
-          component={_logout}
-          name="(login)/_logout"
-          options={{
-            headerShown: true,
-            title: "Logout",
-            drawerLabel: "Logout",
-            drawerIcon: () => (
-              <Icon name="sign-out" size={28} color="#aaa" style={styles.icon} />
-            )
-          }}
-        >
-        </Drawer.Screen>
-      </Drawer.Navigator>
-    </GestureHandlerRootView>
+          <Drawer.Screen
+            name={SCREEN.LOGIN}
+            component={Login}
+            options={{
+              headerShown: false,
+              drawerLabel: () => null,
+              drawerItemStyle: { display: 'none' }
+            }}
+          >
+          </Drawer.Screen>
+          <Drawer.Screen
+            name={SCREEN.Home}
+            component={StackNavigation}
+            options={{
+              headerShown: true,
+              title: "SIGA",
+              drawerLabel: "Home",
+              drawerIcon: () => (
+                <Icon name="home" size={28} color="#aaa" style={styles.icon} />
+              )
+            }}
+          >
+          </Drawer.Screen>
+          <Drawer.Screen
+            component={_forgot}
+            name={SCREEN.FORGOT}
+            options={{
+              headerShown: true,
+              title: "Forgot",
+              drawerLabel: "Forgot",
+              drawerIcon: () => (
+                <Icon name="gear" size={28} color="#aaa" style={styles.icon} />
+              )
+            }}
+          >
+          </Drawer.Screen>
+          <Drawer.Screen
+            component={_caution}
+            name="(component)/_caution"
+            options={{
+              headerShown: true,
+              title: "Avisos",
+              drawerLabel: "Avisos",
+              drawerIcon: () => (
+                <Icon name="thumb-tack" size={28} color="#aaa" style={styles.icon} />
+              )
+            }}
+          >
+          </Drawer.Screen>
+          <Drawer.Screen
+            component={_logout}
+            name="(login)/_logout"
+            options={{
+              headerShown: true,
+              title: "Logout",
+              drawerLabel: "Logout",
+              drawerIcon: () => (
+                <Icon name="sign-out" size={28} color="#aaa" style={styles.icon} />
+              )
+            }}
+          >
+          </Drawer.Screen>
+        </Drawer.Navigator>
+      </GestureHandlerRootView>
+    // </AuthProvider>
   )
 }
 
