38b9127a2c13580061bae4b9f7ab547a1b47fea5
EliasBRodrigues <elias.rborges3@gmail.com>
2025-02-26 23:25:11 -0300

    add: primereact

--- web/gestao-academica-web/src/main.jsx
+++ web/gestao-academica-web/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
+);
