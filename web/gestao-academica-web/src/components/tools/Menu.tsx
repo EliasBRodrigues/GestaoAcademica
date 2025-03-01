7e905fcba810e559342c665786082f5c1205f7a1
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-01 20:59:35 -0300

    add: menu

---
+++ web/gestao-academica-web/src/components/tools/Menu.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from 'react'
+
+import { Toast } from 'primereact/toast';
+import { Sidebar } from "primereact/sidebar";
+import { Button } from 'primereact/button';
+import menu from "../../assets/menu.svg";
+// import { Link } from 'react-router-dom';
+
+const Menu = () => {
+    const [visible, setVisible] = useState(false);
+    const toast = useRef<Toast>(null);
+    const logout = () => {
+        if(toast.current){
+            toast.current?.show({ severity: 'info', summary: 'Loading...', detail: 'User Exit System' });
+        }
+        setTimeout(() => {
+            //userLogout()
+        }, 2000)
+    }
+    return (
+        <div>
+            <Toast ref={toast} position='top-center' />
+            <Sidebar visible={visible} onHide={() => setVisible(false)}>
+                {/* <img src="../../assets/siga.svg" alt="siga" /> */}
+                <Button onClick={logout}>Sair</Button>
+            </Sidebar>
+            <Button onClick={() => setVisible(true)}>
+                <img src={menu} alt="siga" />
+            </Button>
+        </div>
+    )
+}
+
+export default Menu
