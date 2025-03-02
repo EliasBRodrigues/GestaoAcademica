567c1982ad1671cfbadc38d866ae2a6e47a19c8b
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-01 23:58:56 -0300

    comment service until building

--- server/src/main/java/com/gestao_academica_backend/server/service/impl/GradeNotesServiceImpl.java
+++ server/src/main/java/com/gestao_academica_backend/server/service/impl/GradeNotesServiceImpl.java
@@ -1,57 +1,57 @@
-package com.gestao_academica_backend.server.service.impl;
+// package com.gestao_academica_backend.server.service.impl;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.List;
-import java.util.Optional;
+// import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import com.gestao_academica_backend.server.dao.AbstractEntityDao;
-import com.gestao_academica_backend.server.models.Classroom;
-import com.gestao_academica_backend.server.models.Notes;
-import com.gestao_academica_backend.server.models.Students;
-import com.gestao_academica_backend.server.repository.GradeNotesRepository;
-import com.gestao_academica_backend.server.service.GradeNotesService;
+// import com.gestao_academica_backend.server.dao.AbstractEntityDao;
+// import com.gestao_academica_backend.server.models.Classroom;
+// import com.gestao_academica_backend.server.models.Notes;
+// import com.gestao_academica_backend.server.models.Students;
+// import com.gestao_academica_backend.server.repository.GradeNotesRepository;
+// import com.gestao_academica_backend.server.service.GradeNotesService;
 
-@Service
-public class GradeNotesServiceImpl extends AbstractEntityDao<Notes>{
+// @Service
+// public class GradeNotesServiceImpl extends AbstractEntityDao<Notes>{
 
-    private GradeNotesRepository gradeNotesRepository;
+//     private GradeNotesRepository gradeNotesRepository;
 
-    public GradeNotesServiceImpl(GradeNotesRepository gradeNotesRepository) {
-        this.gradeNotesRepository = gradeNotesRepository;
-    }
+//     public GradeNotesServiceImpl(GradeNotesRepository gradeNotesRepository) {
+//         this.gradeNotesRepository = gradeNotesRepository;
+//     }
 
-     public List<Notes> getAllGradeNotes() {
-        return gradeNotesRepository.findAll();
-    }
+//      public List<Notes> getAllGradeNotes() {
+//         return gradeNotesRepository.findAll();
+//     }
 
-    public List<Notes> findByStudent(String id) throws Exception{
-        PreparedStatement pstmt = getConnection().prepareStatement(" SELECT n.*, s.* FROM notes n JOIN students s ON n.student_id = s.id WHERE s.id = ? ");
-        pstmt.setString(1, id);
-        return super.findByPreparedStatement(pstmt);
-    }
+//     public List<Notes> findByStudent(String id) throws Exception{
+//         PreparedStatement pstmt = getConnection().prepareStatement(" SELECT n.*, s.* FROM notes n JOIN students s ON n.student_id = s.id WHERE s.id = ? ");
+//         pstmt.setString(1, id);
+//         return super.findByPreparedStatement(pstmt);
+//     }
 
-    public List<Notes> findGradeNotesByStudentId(Long id) throws Exception{
-        List<Notes> notes = gradeNotesRepository.finGradeNotesByStudentId(id);
-        return notes;
-    }
+//     public List<Notes> findGradeNotesByStudentId(Long id) throws Exception{
+//         List<Notes> notes = gradeNotesRepository.finGradeNotesByStudentId(id);
+//         return notes;
+//     }
 
-    @Override
-    protected Notes resultSetToObject(ResultSet rs) throws Exception {
-        Notes gradeNotes = new Notes();
-        gradeNotes.setId(rs.getLong("id"));
-        //gradeNotes.setSubject(rs.getString("subject"));
-        gradeNotes.setAtividade_um(rs.getDouble("atividade_um"));
-        gradeNotes.setAtividade_dois(rs.getDouble("atividade_dois"));
-        gradeNotes.setAvaliacao_integradora(rs.getDouble("avaliacao_integradora"));
-        gradeNotes.setProjeto_integrador(rs.getDouble("projeto_integrador"));
-        gradeNotes.setMedia(rs.getDouble("media"));
-        Students students = new Students();
-        students.setId(rs.getLong("id"));
-        students.setEmail(rs.getString("email"));
-        gradeNotes.setStudent(students);
-        return gradeNotes;
-    }   
-}
+//     @Override
+//     protected Notes resultSetToObject(ResultSet rs) throws Exception {
+//         Notes gradeNotes = new Notes();
+//         gradeNotes.setId(rs.getLong("id"));
+//         //gradeNotes.setSubject(rs.getString("subject"));
+//         gradeNotes.setAtividade_um(rs.getDouble("atividade_um"));
+//         gradeNotes.setAtividade_dois(rs.getDouble("atividade_dois"));
+//         gradeNotes.setAvaliacao_integradora(rs.getDouble("avaliacao_integradora"));
+//         gradeNotes.setProjeto_integrador(rs.getDouble("projeto_integrador"));
+//         gradeNotes.setMedia(rs.getDouble("media"));
+//         Students students = new Students();
+//         students.setId(rs.getLong("id"));
+//         students.setEmail(rs.getString("email"));
+//         gradeNotes.setStudent(students);
+//         return gradeNotes;
+//     }   
+// }
--- server/src/main/java/com/gestao_academica_backend/server/service/impl/GradeServiceImp.java
+++ server/src/main/java/com/gestao_academica_backend/server/service/impl/GradeServiceImp.java
@@ -1,61 +1,61 @@
-package com.gestao_academica_backend.server.service.impl;
+// package com.gestao_academica_backend.server.service.impl;
 
-import com.gestao_academica_backend.server.dao.AbstractEntityDao;
-import com.gestao_academica_backend.server.models.Classroom;
-import com.gestao_academica_backend.server.models.Grade;
-import com.gestao_academica_backend.server.models.Students;
-import com.gestao_academica_backend.server.repository.GradeRepository;
-import com.gestao_academica_backend.server.service.GradeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import com.gestao_academica_backend.server.dao.AbstractEntityDao;
+// import com.gestao_academica_backend.server.models.Classroom;
+// import com.gestao_academica_backend.server.models.Grade;
+// import com.gestao_academica_backend.server.models.Students;
+// import com.gestao_academica_backend.server.repository.GradeRepository;
+// import com.gestao_academica_backend.server.service.GradeService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.List;
-import java.util.Optional;
-@Service
-public class GradeServiceImp extends AbstractEntityDao<Grade> {
-    private final GradeRepository gradeRepository;
+// import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
+// import java.util.List;
+// import java.util.Optional;
+// @Service
+// public class GradeServiceImp extends AbstractEntityDao<Grade> {
+//     private final GradeRepository gradeRepository;
 
-    @Autowired
-    public GradeServiceImp(GradeRepository gradeRepository) {
-        this.gradeRepository = gradeRepository;
-    }
+//     @Autowired
+//     public GradeServiceImp(GradeRepository gradeRepository) {
+//         this.gradeRepository = gradeRepository;
+//     }
 
-    //@Override
-    public List<Grade> getAllGrades() {
-        return gradeRepository.findAll();
-    }
+//     //@Override
+//     public List<Grade> getAllGrades() {
+//         return gradeRepository.findAll();
+//     }
 
-    //@Override
-    public Optional<Grade> getGradeById(Long id) {
-        return gradeRepository.findById(id);
-    }
+//     //@Override
+//     public Optional<Grade> getGradeById(Long id) {
+//         return gradeRepository.findById(id);
+//     }
 
-    public List<Grade> getGradeByClassroomId(Long id){
-        List<Grade> grades = gradeRepository.findGradesByClassroomId(id);
-        return grades;
-        //return grades.orElseThrow(() -> new RuntimeException("Grade not found"));
-    }
+//     public List<Grade> getGradeByClassroomId(Long id){
+//         List<Grade> grades = gradeRepository.findGradesByClassroomId(id);
+//         return grades;
+//         //return grades.orElseThrow(() -> new RuntimeException("Grade not found"));
+//     }
 
-    // public List<Grade> findByGradeStudenList(Long id) throws Exception{
-    //     PreparedStatement pstmt = getConnection().prepareStatement(" SELECT g.*, s.* FROM grade g JOIN students s ON g.id = s.id WHERE s.id = ? ");
-    //     pstmt.setLong(1, id);
-    //     return super.findByPreparedStatement(pstmt);
-    // }
+//     // public List<Grade> findByGradeStudenList(Long id) throws Exception{
+//     //     PreparedStatement pstmt = getConnection().prepareStatement(" SELECT g.*, s.* FROM grade g JOIN students s ON g.id = s.id WHERE s.id = ? ");
+//     //     pstmt.setLong(1, id);
+//     //     return super.findByPreparedStatement(pstmt);
+//     // }
 
-    @Override
-    protected Grade resultSetToObject(ResultSet rs) throws Exception {
-        Grade grade = new Grade();
-        grade.setId(rs.getLong("id"));
-        grade.setNameGrade(rs.getString("name_grade"));
-        grade.setSigla(rs.getString("sigla"));
-        grade.setSubjectHours(rs.getString("subject_hours"));
-        grade.setSubjectFormatClass(rs.getString("subject_format_class"));
-        grade.setHorary(rs.getString("horary"));
-        Students students = new Students();
-        students.setId(rs.getLong("id"));
-        // grade.setStudent(students);
-        return grade;
-    }
-}
+//     @Override
+//     protected Grade resultSetToObject(ResultSet rs) throws Exception {
+//         Grade grade = new Grade();
+//         grade.setId(rs.getLong("id"));
+//         grade.setNameGrade(rs.getString("name_grade"));
+//         grade.setSigla(rs.getString("sigla"));
+//         grade.setSubjectHours(rs.getString("subject_hours"));
+//         grade.setSubjectFormatClass(rs.getString("subject_format_class"));
+//         grade.setHorary(rs.getString("horary"));
+//         Students students = new Students();
+//         students.setId(rs.getLong("id"));
+//         // grade.setStudent(students);
+//         return grade;
+//     }
+// }
