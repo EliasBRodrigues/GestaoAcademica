1597424f58b57de1236357e0b395bf24cd83a43b
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-01 23:58:25 -0300

    comment controller until building

--- server/src/main/java/com/gestao_academica_backend/server/controller/ClassroomController.java
+++ server/src/main/java/com/gestao_academica_backend/server/controller/ClassroomController.java
@@ -1,43 +1,43 @@
-package com.gestao_academica_backend.server.controller;
+// package com.gestao_academica_backend.server.controller;
 
-import com.gestao_academica_backend.server.constants.ClassroomEnumeration;
-import com.gestao_academica_backend.server.models.Classroom;
-import com.gestao_academica_backend.server.service.ClassroomService;
-import com.gestao_academica_backend.server.service.impl.ClassroomServiceImp;
+// import com.gestao_academica_backend.server.constants.ClassroomEnumeration;
+// import com.gestao_academica_backend.server.models.Classroom;
+// import com.gestao_academica_backend.server.service.ClassroomService;
+// import com.gestao_academica_backend.server.service.impl.ClassroomServiceImp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@CrossOrigin(origins = "http://localhost:8081/")
-public class ClassroomController {
+// @RestController
+// @CrossOrigin(origins = "http://localhost:8081/")
+// public class ClassroomController {
 
-    private final ClassroomServiceImp classroomService;
+//     private final ClassroomServiceImp classroomService;
 
-    @Autowired
-    public ClassroomController(ClassroomServiceImp classroomService) {
-        this.classroomService = classroomService;
-    }
+//     @Autowired
+//     public ClassroomController(ClassroomServiceImp classroomService) {
+//         this.classroomService = classroomService;
+//     }
 
-    @GetMapping("/api/classrooms")
-    public ResponseEntity<List<Classroom>> getAllClassrooms() {
-        List<Classroom> classrooms = classroomService.getAllClassrooms();
-        return ResponseEntity.ok(classrooms);
-    }
+//     @GetMapping("/api/classrooms")
+//     public ResponseEntity<List<Classroom>> getAllClassrooms() {
+//         List<Classroom> classrooms = classroomService.getAllClassrooms();
+//         return ResponseEntity.ok(classrooms);
+//     }
 
-    @GetMapping("/api/classrooms/{id}")
-    public ResponseEntity<?> getClassroomById(@PathVariable String id) throws Exception {
-        return ResponseEntity.ok(classroomService.findByClassroomGrade(id));
-    }
+//     @GetMapping("/api/classrooms/{id}")
+//     public ResponseEntity<?> getClassroomById(@PathVariable String id) throws Exception {
+//         return ResponseEntity.ok(classroomService.findByClassroomGrade(id));
+//     }
 
     
 
-    // @GetMapping("/api/classroom/{classrooms}")
-    // public List<Classroom> getClassroomsByName(@PathVariable("classrooms") String classrooms) {
-    //     ClassroomEnumeration classroom = ClassroomEnumeration.valueOf(classrooms);
-    //     return classroomService.findByNameClassrooms(classroom);
-    // }
-}
+//     // @GetMapping("/api/classroom/{classrooms}")
+//     // public List<Classroom> getClassroomsByName(@PathVariable("classrooms") String classrooms) {
+//     //     ClassroomEnumeration classroom = ClassroomEnumeration.valueOf(classrooms);
+//     //     return classroomService.findByNameClassrooms(classroom);
+//     // }
+// }
--- server/src/main/java/com/gestao_academica_backend/server/controller/GradeController.java
+++ server/src/main/java/com/gestao_academica_backend/server/controller/GradeController.java
@@ -1,61 +1,61 @@
-package com.gestao_academica_backend.server.controller;
+// package com.gestao_academica_backend.server.controller;
 
-import com.gestao_academica_backend.server.models.Classroom;
-import com.gestao_academica_backend.server.models.Grade;
-import com.gestao_academica_backend.server.models.Section;
-import com.gestao_academica_backend.server.service.ClassroomService;
-import com.gestao_academica_backend.server.service.GradeService;
-import com.gestao_academica_backend.server.service.SectionService;
-import com.gestao_academica_backend.server.service.impl.ClassroomServiceImp;
-import com.gestao_academica_backend.server.service.impl.GradeServiceImp;
+// import com.gestao_academica_backend.server.models.Classroom;
+// import com.gestao_academica_backend.server.models.Grade;
+// import com.gestao_academica_backend.server.models.Section;
+// import com.gestao_academica_backend.server.service.ClassroomService;
+// import com.gestao_academica_backend.server.service.GradeService;
+// import com.gestao_academica_backend.server.service.SectionService;
+// import com.gestao_academica_backend.server.service.impl.ClassroomServiceImp;
+// import com.gestao_academica_backend.server.service.impl.GradeServiceImp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@CrossOrigin
-public class GradeController {
+// @RestController
+// @CrossOrigin
+// public class GradeController {
 
-    private final GradeServiceImp gradeService;
-    private final ClassroomServiceImp classroomService;
-    private final SectionService sectionService;
+//     private final GradeServiceImp gradeService;
+//     private final ClassroomServiceImp classroomService;
+//     private final SectionService sectionService;
 
-    @Autowired
-    public GradeController(GradeServiceImp gradeService, ClassroomServiceImp classroomService, SectionService sectionService)
-    {
-        this.gradeService = gradeService;
-        this.classroomService = classroomService;
-        this.sectionService = sectionService;
-    }
+//     @Autowired
+//     public GradeController(GradeServiceImp gradeService, ClassroomServiceImp classroomService, SectionService sectionService)
+//     {
+//         this.gradeService = gradeService;
+//         this.classroomService = classroomService;
+//         this.sectionService = sectionService;
+//     }
 
-    @GetMapping("/api/grades")
-    public ResponseEntity<List<Grade>> getAllGrades() {
-        List<Grade> grades = gradeService.getAllGrades();
-        return ResponseEntity.ok(grades);
-    }
+//     @GetMapping("/api/grades")
+//     public ResponseEntity<List<Grade>> getAllGrades() {
+//         List<Grade> grades = gradeService.getAllGrades();
+//         return ResponseEntity.ok(grades);
+//     }
 
-    // @GetMapping("/api/classrooms/grade={id}")
-    // public ResponseEntity<List<Classroom>> getAllClassroomsByGradeId(@PathVariable Long id) {
-    //     return ResponseEntity.ok(classroomService.findByGradeStudenList(id));
-    // }
+//     // @GetMapping("/api/classrooms/grade={id}")
+//     // public ResponseEntity<List<Classroom>> getAllClassroomsByGradeId(@PathVariable Long id) {
+//     //     return ResponseEntity.ok(classroomService.findByGradeStudenList(id));
+//     // }
 
-    // @GetMapping("/api/grades/{id}")
-    // public ResponseEntity<Grade> getGradeById(@PathVariable Long id) {
-    //     return gradeService.getGradeById(id)
-    //             .map(ResponseEntity::ok)
-    //             .orElse(ResponseEntity.notFound().build());
-    // }
+//     // @GetMapping("/api/grades/{id}")
+//     // public ResponseEntity<Grade> getGradeById(@PathVariable Long id) {
+//     //     return gradeService.getGradeById(id)
+//     //             .map(ResponseEntity::ok)
+//     //             .orElse(ResponseEntity.notFound().build());
+//     // }
 
-    // @GetMapping("/api/grades/{id}")
-    // public ResponseEntity<?> getGradeByStudentId(@PathVariable Long id) throws Exception {
-    //     return ResponseEntity.ok(gradeService.findByGradeStudenList(id));
-    // }
+//     // @GetMapping("/api/grades/{id}")
+//     // public ResponseEntity<?> getGradeByStudentId(@PathVariable Long id) throws Exception {
+//     //     return ResponseEntity.ok(gradeService.findByGradeStudenList(id));
+//     // }
 
-    @GetMapping("/api/grade/classroom/{id}")
-    public ResponseEntity<?> getGradeByClassroomId(@PathVariable Long id) throws Exception {
-        return ResponseEntity.ok(gradeService.getGradeByClassroomId(id));
-    }
-}
+//     @GetMapping("/api/grade/classroom/{id}")
+//     public ResponseEntity<?> getGradeByClassroomId(@PathVariable Long id) throws Exception {
+//         return ResponseEntity.ok(gradeService.getGradeByClassroomId(id));
+//     }
+// }
