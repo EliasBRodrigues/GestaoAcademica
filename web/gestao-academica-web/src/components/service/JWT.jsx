2917a07896489cac8bb3163f71bbf6c62d2f69d3
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-04 01:24:17 -0300

    parseJWT

---
+++ web/gestao-academica-web/src/components/service/JWT.jsx
@@ -0,0 +1,25 @@
+export function parseJWT(token){
+    if (!token || token.split('.').length !== 3) { // Verificando se o token tem o formato correto
+        console.error('Token inválido ou malformado');
+        return null;
+    }
+
+    try {
+        const base64URL = token.split('.')[1];
+        const base64 = base64URL.replace('-', '+').replace('_', '/');
+        return JSON.parse(window.atob(base64));
+    } catch (error) {
+        console.error('Erro ao decodificar o JWT', error);
+        return null;
+    }
+}
+
+export const handleLogError = (error) => {
+    if(error.response){
+        console.log(error.response.data)
+    } else if (error.request){
+        console.log(error.request)
+    } else {
+        console.log(error.message)
+    }
+}
