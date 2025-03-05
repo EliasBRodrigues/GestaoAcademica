15ca2ae918369f844d8d35e2d65c77f690e27f45
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:24:41 -0300

    add: functions search email users

--- server/src/main/java/com/gestao_academica_backend/server/repository/StudentRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/StudentRepository.java
@@ -10,12 +10,9 @@ import com.gestao_academica_backend.server.models.Grade;
 import com.gestao_academica_backend.server.models.Students;
 
 public interface StudentRepository extends JpaRepository<Students,Long> {
-    // @Query("SELECT AVG(g.media) FROM GradeNotes g WHERE g.student.id = :studentId")
-    // double calculateStudentAverageGrade(@Param("studentId") Long studentId);
-
-    // @Query("SELECT s FROM Students s WHERE s.grade_id = :id")
-    // List<Students> findGradeByStudentId(@Param("id") Long id);
-
     @Query("SELECT s FROM Students s WHERE s.user =:id")
     List<Students> findStudentsByUserId(@Param("id") Long id);
+
+    @Query("SELECT s FROM Students s WHERE s.email = :email")
+    Students findStudentsByUserEmail(@Param("email") String email);
 }
