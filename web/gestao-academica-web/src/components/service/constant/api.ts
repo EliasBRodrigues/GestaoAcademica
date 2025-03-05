965bbdec8ff4a4399d77f19c41db176f32c349bc
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:30:37 -0300

    update: api file with time & logout by token limit

--- web/gestao-academica-web/src/components/service/constant/api.ts
+++ web/gestao-academica-web/src/components/service/constant/api.ts
@@ -1,8 +1,30 @@
 import axios, { AxiosInstance } from 'axios';
+import { parseJWT } from '../JWT';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/',
-    timeout: 10000,
+  baseURL: 'http://localhost:8080/',
+  timeout: 10000,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const token = user?.accessToken;
+
+    if (token) {
+      const data = parseJWT(token);
+      if (Date.now() > data.exp * 10000) {
+        window.location.href = '/';
+        return Promise.reject('Token expirado');
+      }
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
