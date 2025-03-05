3ccefc5ff7b90e45357d1ba7fd20788239223eff
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:21:56 -0300

    refactor all controller of Notes

--- server/src/main/java/com/gestao_academica_backend/server/controller/GradeNotesController.java
+++ server/src/main/java/com/gestao_academica_backend/server/controller/GradeNotesController.java
@@ -1,40 +1,44 @@
-// package com.gestao_academica_backend.server.controller;
+package com.gestao_academica_backend.server.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-// import com.gestao_academica_backend.server.models.Classroom;
-// import com.gestao_academica_backend.server.models.Notes;
-// import com.gestao_academica_backend.server.service.impl.GradeNotesServiceImpl;
+import com.gestao_academica_backend.server.models.Notes;
+import com.gestao_academica_backend.server.service.impl.GradeNotesServiceImpl;
 
-// @Controller
-// @CrossOrigin(origins = "http://localhost:8081/")
-// public class GradeNotesController {
-//     private GradeNotesServiceImpl gradeNotesServiceImpl;
+@Controller
+@CrossOrigin(origins = "http://localhost:5173/")
+public class GradeNotesController {
+    private GradeNotesServiceImpl gradeNotesServiceImpl;
 
-//     public GradeNotesController(GradeNotesServiceImpl gradeNotesServiceImpl) {
-//         this.gradeNotesServiceImpl = gradeNotesServiceImpl;
-//     }
+    public GradeNotesController(GradeNotesServiceImpl gradeNotesServiceImpl) {
+        this.gradeNotesServiceImpl = gradeNotesServiceImpl;
+    }
 
-//     @GetMapping("/api/gradesNotes")
-//     public ResponseEntity<List<Notes>> getAllGradesNotes() {
-//         List<Notes> gradesNotes = gradeNotesServiceImpl.getAllGradeNotes();
-//         return ResponseEntity.ok(gradesNotes);
-//     }
+    @GetMapping("/api/gradesNotes")
+    public ResponseEntity<List<Notes>> getAllGradesNotes() {
+        List<Notes> gradesNotes = gradeNotesServiceImpl.getAllGradeNotes();
+        return ResponseEntity.ok(gradesNotes);
+    }
 
-//     @GetMapping("/api/gradesNotes/{id}")
-//     public ResponseEntity<?> getClassroomById(@PathVariable String id) throws Exception {
-//         return ResponseEntity.ok(gradeNotesServiceImpl.findByStudent(id));
-//     }
+    @GetMapping("/api/gradesNotes/{id}")
+    public ResponseEntity<?> getClassroomById(@PathVariable String id) throws Exception {
+        return ResponseEntity.ok(gradeNotesServiceImpl.findByStudent(id));
+    }
 
-//     @GetMapping("/api/grades/notes/{id}")
-//     public ResponseEntity<?> getGradesNotesByStudentId(@PathVariable Long id) throws Exception{
-//         return ResponseEntity.ok(gradeNotesServiceImpl.findGradeNotesByStudentId(id));
-//     }
+    @GetMapping("/api/grades/notes/{id}")
+    public ResponseEntity<?> getGradesNotesByStudentId(@PathVariable Long id) throws Exception{
+        return ResponseEntity.ok(gradeNotesServiceImpl.findGradeNotesByStudentId(id));
+    }
 
-// }
+    @GetMapping("/api/grades/student/notes/{email}")
+    public ResponseEntity<?> getGradesNotesByStudentEmail(@PathVariable String email) throws Exception{
+        return ResponseEntity.ok(gradeNotesServiceImpl.findGradeNotesByStudentEmail(email));
+    }
+
+}
