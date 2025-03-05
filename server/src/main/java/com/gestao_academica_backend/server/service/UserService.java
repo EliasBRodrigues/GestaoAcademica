04641e8f7ec9c3a884b0ca598d181b4d1803391b
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:27:34 -0300

    add: method getUserByEmail interface

--- server/src/main/java/com/gestao_academica_backend/server/service/UserService.java
+++ server/src/main/java/com/gestao_academica_backend/server/service/UserService.java
@@ -10,6 +10,7 @@ import com.gestao_academica_backend.server.models.User;
 public interface UserService {
     List<User> getAllUsers();
     Optional<User> getUserByUsername(String username);
+    Optional<User> getUserByEmail(String email);
     Boolean hasUserWithUsername(String username);
     Boolean hasUserWithEmail(String email);
     User validateAndGetUserByUsername(String username);
