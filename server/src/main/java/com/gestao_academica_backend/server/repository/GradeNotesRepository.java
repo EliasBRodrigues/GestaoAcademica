5aa32295221fc4bc835aab552ab376858003468d
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:24:26 -0300

    feat: constructor Query and consuming DTO

--- server/src/main/java/com/gestao_academica_backend/server/repository/GradeNotesRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/GradeNotesRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.gestao_academica_backend.server.models.Notes;
+import com.gestao_academica_backend.server.models.dto.NotesDTO;
 
 public interface GradeNotesRepository extends JpaRepository<Notes, Long>{
     @Query("SELECT n FROM Notes n JOIN n.student s WHERE s.id = :id")
     List<Notes> finGradeNotesByStudentId(@Param("id") Long id);
+
+    @Query("SELECT new com.gestao_academica_backend.server.models.dto.NotesDTO(s.email, n.subject, n.atividade_um, n.atividade_dois, n.projeto_integrador, n.avaliacao_integradora, n.media) FROM Notes n JOIN Students s ON n.student.id = s.id WHERE s.email LIKE %:email%")
+    List<NotesDTO> finGradeNotesByStudentEmail(@Param("email") String email);
 }
--- server/src/main/java/com/gestao_academica_backend/server/repository/GradeRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/GradeRepository.java
@@ -8,8 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.gestao_academica_backend.server.models.Grade;
+import com.gestao_academica_backend.server.models.dto.GradeDTO;
 
 public interface GradeRepository extends JpaRepository<Grade,Long> {
     @Query("SELECT g FROM Grade g WHERE g.classroom.id = :id")
     List<Grade> findGradesByClassroomId(@Param("id") Long id);
+
+    @Query("SELECT new com.gestao_academica_backend.server.models.dto.GradeDTO(g.nameGrade, g.sigla, g.horary, s.email) FROM Grade g JOIN Students s ON g.classroom.id = s.classroom.id WHERE s.email LIKE %:email%")
+    List<GradeDTO> findGradesByStudenList(@Param("email") String email);
 }
