7690a88d8f57bf4db334e7512f97c1c6b2c36d66
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 00:30:13 -0300

    feat: set data screen Student

--- web/gestao-academica-web/src/components/screens/Student.tsx
+++ web/gestao-academica-web/src/components/screens/Student.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ButtonInterface from '../tools/ButtonInterface'
 import profile from '../../assets/profile.svg';
 import Menu from '../tools/Menu';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../auth/context/AuthContext';
+import { Students } from '../interface/Students';
+import { getUserByEmail } from '../service/Requests';
 
 const Student = () => {
     const nav = useNavigate();
+    const Auth = useAuth();
+    const user = Auth.getUser();
+    const [students, setStudent] = useState<Students | null>(null);
+    const emailStudent = user.data.email;
+    
+    const fetchData = async () => {
+        try {
+            const response = await getUserByEmail(emailStudent);
+            setStudent(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+      }, [])
     return (
         <div className='flex flex-col items-center justify-center'>
             <div className='bg-[#709BEF] w-full h-85 flex flex-col'>
                 <Menu />
                 <img src={profile} className='block ml-auto mr-auto mb-5' />
-                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>aluno</h3>
-                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>email</h3>
-                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>ra</h3>
+                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>{students?.name}</h3>
+                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>{students?.email}</h3>
+                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>{students?.ra}</h3>
             </div>
             <div className='pt-1 grid grid-cols-2 gap-7 justify-center w-full max-w-sm'>
                 <ButtonInterface style={{'backgroundColor': '#709BEF'}} size={'large'} icon={'pi pi-id-card'} position={'top'} label='Matrícula' props={() => nav('/student/registration')}/>
