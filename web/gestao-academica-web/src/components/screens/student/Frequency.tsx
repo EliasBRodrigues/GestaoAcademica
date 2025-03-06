1f69ad30b6562c17fc9c89d1f331d264e7400714
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-05 23:36:50 -0300

    feat: conuming backend and receive data from database

--- web/gestao-academica-web/src/components/screens/student/Frequency.tsx
+++ web/gestao-academica-web/src/components/screens/student/Frequency.tsx
@@ -1,48 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Menu from '../../tools/Menu'
+import { Attendance } from '../../interface/Attendance';
+import { useAuth } from '../../auth/context/AuthContext';
+import { getAttendancesByEmail } from '../../service/Requests';
 
 const Frequency = () => {
-    const getRandomInt = (min: number, max: number) => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      };
-      
-      const subjects = ['IoT', 'PM', 'BD', 'AI', 'SE'];
-      
-      return (
-        <div className='flex flex-col items-center justify-center'>
-            <div className='bg-[#709BEF] w-full h-42 flex flex-col'>
-                <Menu />
-                <h3 className='text-center mt-10 text-amber-50 text-4xl'>Frequência</h3>
-            </div>
-          <div className="flex-shrink-0 bg-[#709BEF] h-10"></div>
-          <div className="flex-grow bg-white rounded-t-[50px] pt-8 px-5 pb-4">
-            <div className="w-full bg-white">
-              <div className="w-full border-t-[2px] border-[#709BEF]">
-                <div className="flex items-center border-b-[1px] border-[#709BEF] py-2 px-4">
-                  <p className="flex-1 text-center font-semibold text-lg text-black">MATERIA</p>
-                  <div className="w-[1px] h-full bg-[#709BEF]"></div>
-                  <p className="flex-1 text-center font-semibold text-lg text-black">PRESENÇA</p>
-                  <div className="w-[1px] h-full bg-[#709BEF]"></div>
-                  <p className="flex-1 text-center font-semibold text-lg text-black">FALTAS</p>
-                </div>
-      
-                {subjects.map((subject, index) => (
-                  <div key={index} className="flex items-center border-b-[1px] border-[#709BEF] py-2 px-4">
-                    <p className="flex-1 text-center text-black">{subject}</p>
-                    <div className="w-[1px] h-full bg-[#709BEF]"></div>
-                    <p className="flex-1 text-center text-black">{getRandomInt(10, 20)}</p>
-                    <div className="w-[1px] h-full bg-[#709BEF]"></div>
-                    <p className="flex-1 text-center text-black">{getRandomInt(0, 5)}</p>
-                  </div>
-                ))}
-              </div>
-              <p className="text-black text-sm font-normal mt-4 px-4 text-center">
-                Obs: Caso atinja um percentual acima de 25% na matéria será reprovado por falta na matéria.
-              </p>
-            </div>
-          </div>
-        </div>
-      );
+
+  const Auth = useAuth();
+  const user = Auth.getUser();
+  const email = user.data.email;
+
+  const [attendances, setAttendances] = useState<string | Attendance | null>(null);
+  const fetchData = async () => {
+    try {
+      const response = getAttendancesByEmail(email);
+      setAttendances(await response);
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [email]);
+
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <div className='bg-[#709BEF] w-full h-42 flex flex-col'>
+        <Menu />
+        <h3 className='text-center mt-10 text-amber-50 text-4xl'>Frequência</h3>
+      </div>
+      <div className="flex-shrink-0 bg-[#709BEF] h-10"></div>
+      <div className="flex-grow bg-white rounded-t-[50px] pt-8 px-5 pb-4">
+        <div className="w-full bg-white">
+          <div className="w-full border-t-[2px] border-[#709BEF]">
+            <div className="flex items-center border-b-[1px] border-[#709BEF] py-2 px-4">
+              <p className="flex-1 text-center font-semibold text-lg text-black">MATERIA</p>
+              <div className="w-[1px] h-full bg-[#709BEF]"></div>
+              <p className="flex-1 text-center font-semibold text-lg text-black">PRESENÇA</p>
+              <div className="w-[1px] h-full bg-[#709BEF]"></div>
+              <p className="flex-1 text-center font-semibold text-lg text-black">FALTAS</p>
+            </div>
+            {attendances && Array.isArray(attendances) ? (
+              attendances.map((item: Attendance, idx) => (
+                <div key={idx} className="flex items-center border-b-[1px] border-[#709BEF] py-2 px-4">
+                  <p className="flex-1 text-center text-black">{item.sigla}</p>
+                  <div className="w-[1px] h-full bg-[#709BEF]"></div>
+                  <p className="flex-1 text-center text-black">{item.attendancePresent}</p>
+                  <div className="w-[1px] h-full bg-[#709BEF]"></div>
+                  <p className="flex-1 text-center text-black">{item.attendanceAbsent}</p>
+                </div>
+              ))
+            ) : (
+              <div className="text-center text-gray-500">Nenhum horário disponível.</div>
+            )}
+          </div>
+          <p className="text-black text-sm font-normal mt-4 px-4 text-center">
+            Obs: Caso atinja um percentual acima de 25% na matéria será reprovado por falta na matéria.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Frequency
--- web/gestao-academica-web/src/components/screens/student/subcomponent/Subjects.tsx
+++ web/gestao-academica-web/src/components/screens/student/subcomponent/Subjects.tsx
@@ -1,23 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ButtonGeneral from '../../../tools/ButtonGeneral'
 import Menu from '../../../tools/Menu'
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
-
-interface City {
-    name: string;
-    code: string;
-}
+import { Subject } from '../../../interface/Subject';
+import { Semester } from '../../../interface/Semester';
+import { getClassroomGradesByGradeId } from '../../../service/Requests';
+import { Toast } from 'primereact/toast';
+import { useNavigate } from 'react-router-dom';
 
 const Subjects = () => {
-    const [selectedCity, setSelectedCity] = useState<City | null>(null);
-    const cities: City[] = [
-        { name: 'DSM 1', code: '1' },
-        { name: 'DSM 2', code: '2' },
-        { name: 'DSM 3', code: '3' },
-        { name: 'DSM 4', code: '4' },
-        { name: 'DSM 5', code: '5' },
-        { name: 'DSM 6', code: '6' }
+    const nav = useNavigate();
+    const toast = useRef<Toast>(null);
+    const [selected, setSelected] = useState<Semester | null>();
+    const [subject, setSubject] = useState<string | Subject[] | null>([]);
+
+    const fetchData = async (semester: string) => {
+        try {
+            const response = await getClassroomGradesByGradeId(semester);
+            setSubject(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handlePickerChange = (item: Semester) => {
+        setSelected(item);
+        fetchData(item.id);
+    };
+
+    const showSuccess = () => {
+        toast.current?.show({ severity: 'info', summary: 'Info', detail: 'Carregando', life: 3000 });
+        toast.current?.show({ severity: 'success', summary: 'Success', detail: 'Novas matérias e um novo semestre!', life: 5000 });
+        setTimeout(() => {
+            nav('/student');
+        }, 5000)
+    }
+
+    const semesters: Semester[] = [
+        { id: 'DSM_1', classrooms: '1º Semestre' },
+        { id: 'DSM_2', classrooms: '2º Semestre' },
+        { id: 'DSM_3', classrooms: '3º Semestre' },
+        { id: 'DSM_4', classrooms: '4º Semestre' },
+        { id: 'DSM_5', classrooms: '5º Semestre' },
+        { id: 'DSM_6', classrooms: '6º Semestre' },
     ];
+
+    useEffect(() => {
+        if (selected) {
+            fetchData(selected.id);
+        }
+    }, [selected]);
+
     return (
         <div className='flex flex-col items-center justify-center'>
             <div className='bg-[#709BEF] w-full h-42 flex flex-col'>
@@ -25,13 +58,27 @@ const Subjects = () => {
                 <h3 className='text-center mt-10 text-amber-50 text-4xl'>Matérias</h3>
             </div>
             <div>
+                <Toast ref={toast} />
                 <div className="card flex justify-content-center mb-5 mt-5">
-                    <Dropdown value={selectedCity} onChange={(e: DropdownChangeEvent) => setSelectedCity(e.value)} options={cities} optionLabel="name"
+                    <Dropdown value={selected} onChange={(e: DropdownChangeEvent) => handlePickerChange(e.value)} options={semesters} optionLabel="classrooms"
                         placeholder="Selecione um semestre" className="w-full md:w-14rem" />
                 </div>
-
-
-                <ButtonGeneral label='Matrícula' submit={undefined} click={undefined} styles={{ 'backgroundColor': '#709BEF' }} />
+                <div className="overflow-y-auto max-h-[300px]">
+                    {subject && Array.isArray(subject) ? (
+                        subject.map((item: any, index: number) => (
+                            <div key={index}
+                                className="bg-white p-4 mb-2 rounded-lg shadow-lg"
+                            >
+                                <h4 className="text-lg font-bold mb-2">{item.nameGrade}</h4>
+                                <p>{`Quantidade de aulas: ${item.subjectHours}`}</p>
+                                <p>{`Modelo: ${item.subjectFormatClass}`}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-base text-gray-800">{subject}</p>
+                    )}
+                </div>
+                <ButtonGeneral label='Matrícula' submit={undefined} click={showSuccess} styles={{ 'backgroundColor': '#709BEF' }} />
             </div>
         </div>
     )
