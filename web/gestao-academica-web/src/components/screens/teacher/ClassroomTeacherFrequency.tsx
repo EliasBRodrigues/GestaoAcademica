a681bb7eeddb88a1f9e29186459800da53079be9
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-15 21:34:53 -0300

    feat: consuming data to table and return student of subjects

--- web/gestao-academica-web/src/components/screens/teacher/ClassroomTeacherFrequency.tsx
+++ web/gestao-academica-web/src/components/screens/teacher/ClassroomTeacherFrequency.tsx
@@ -1,20 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Menu from '../../tools/Menu';
 import { useNavigate } from 'react-router-dom';
 import ButtonInterface from '../../tools/ButtonInterface';
+import style from '../../tools/style/input-button.module.css';
+import Loader from '../../tools/Loader';
+import { useAuth } from '../../auth/context/AuthContext';
+import { Teachers } from '../../interface/Teachers';
+import { getClassroomTeachers } from '../../service/Requests';
 
 const ClassromTeacherFrequency = () => {
     const nav = useNavigate();
-    return (
-        <div className='flex flex-col items-center justify-center'>
-            <div className='bg-orange-400 w-full h-42 flex flex-col'>
-                <Menu />
-                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Selecione a turma</h3>
-                <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Frequência</h3>
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const Auth = useAuth();
+    const user = Auth.getUser();
+    const [teacher, setTeacher] = useState<Teachers[]>([]);
+    const emailTeacher = user.data.email;
+
+    const fetchData = async () => {
+        try {
+            if (emailTeacher) {
+                const response = await getClassroomTeachers(emailTeacher);
+                const teacherData = response.map((item: any) => ({
+                    email: item[0],
+                    name: item[1],
+                    nameSection: item[2],
+                    nameGrade: item[3],
+                    sigla: item[4]
+                }));
+                setTeacher(teacherData);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setTimeout(() => {
+                setLoading(false);
+            }, 700);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [emailTeacher]);
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <Loader isLoading={loading} />
             </div>
-            <div className='pt-1 grid grid-cols-2 gap-7 justify-center w-full max-w-sm'>
-                <ButtonInterface style={{ 'backgroundColor': 'orange' }} size={'large'} icon={'pi pi-users'} position={'top'} label='DSM 1' props={() => nav('/teacher/classroom/frequency/freq')} />
-                <ButtonInterface style={{ 'backgroundColor': 'orange' }} size={'large'} icon={'pi pi-users'} position={'top'} label='DSM 2' props={() => nav('/teacher/classroom/frequency/freq')} />
+        )
+    }
+
+    if (!teacher) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <p>Erro ao carregar os dados do professor.</p>
+                <p>Tente novamente mais tarde.</p>
+            </div>
+        );
+    }
+
+    const handleMateriaClick = (nameGrade: string, nameSection: string, sigla: string) => {
+        nav(`/teacher/classroom/frequency/freq`, {
+            state: { nameGrade, nameSection, sigla }
+        });
+    };
+
+
+    return (
+        <div className={style.fade_in}>
+            <div className='flex flex-col items-center justify-center'>
+                <div className='bg-orange-400 w-full h-42 flex flex-col mb-10'>
+                    <Menu />
+                    <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Selecione a turma</h3>
+                    <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Frequência</h3>
+                </div>
+                <div className='pt-1 grid grid-cols-2 gap-7 justify-center w-full max-w-sm'>
+                    {teacher && Array.isArray(teacher) ? (
+                        teacher.map((teacher, index) => (
+                            <div key={index}>
+                                <h3 className='border border-amber-600 bg-white w-full sm:w-[100%] p-2 mb-4 rounded-lg flex flex-col items-center justify-center'>{teacher.nameSection}</h3>
+                                <ButtonInterface style={{ 'backgroundColor': 'orange' }} size={'large'} icon={'pi pi-users'} position={'top'} label={teacher.nameGrade} props={() => handleMateriaClick(teacher.nameGrade, teacher.nameSection, teacher.sigla)} />
+                            </div>
+                        ))
+                    ) : (
+                        <div>Não foi possível carregar as informações</div>
+                    )}
+                </div>
             </div>
         </div>
     )
--- web/gestao-academica-web/src/components/screens/teacher/subcomponents/FrequencyTeacher.tsx
+++ web/gestao-academica-web/src/components/screens/teacher/subcomponents/FrequencyTeacher.tsx
@@ -4,31 +4,46 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import ButtonGeneral from '../../../tools/ButtonGeneral';
 import { Toast } from 'primereact/toast';
-import { useNavigate } from 'react-router-dom';
-
-interface Student {
-  id?: string;
-  name?: string;
-}
+import { useLocation, useNavigate } from 'react-router-dom';
+import { StudentGrade } from '../../../interface/StudentGrade';
+import useTeacherData from '../../../../hook/TeacherData';
+import { useAuth } from '../../../auth/context/AuthContext';
+import { getSubjectsByStudent } from '../../../service/Requests';
+import style from '../../../tools/style/input-button.module.css'
+import Loader from '../../../tools/Loader';
 
 const FrequencyTeacher = () => {
   const nav = useNavigate();
+  const { state } = useLocation();
+  const { sigla, nameGrade } = state || {};  
   const toast = useRef<Toast>(null);
-  const [student, setstudent] = useState<Student[]>([]);
-  const [selectedstudent, setSelectedstudent] = useState<Student[] | null>(null);
+
+  const Auth = useAuth();
+  const user = Auth.getUser();
+  const { teacher, loading } = useTeacherData(user.data.email);
+
+  const [student, setstudent] = useState<StudentGrade[]>([]);
+  const [selectedstudent, setSelectedstudent] = useState<StudentGrade[] | null>(null);
   const [rowClick, setRowClick] = useState<boolean>(true);
 
-  const studentList = [
-    { id: '0', name: 'Aluno 1' },
-    { id: '1', name: 'Aluno 2' },
-    { id: '2', name: 'Aluno 3' },
-    { id: '3', name: 'Aluno 4' },
-    { id: '4', name: 'Aluno 5' },
-    { id: '4', name: 'Aluno 6' },
-    { id: '4', name: 'Aluno 7' },
-    { id: '4', name: 'Aluno 8' },
-    { id: '4', name: 'Aluno 9' },
-  ]
+  useEffect(() => {
+    if (teacher && teacher.length > 0) {
+      fetchData();
+    }
+  }, [teacher]);
+
+  const fetchData = async () => {
+    try {
+      const response = await getSubjectsByStudent(sigla);
+      const formattedResponse = response.map(([id, name]) => ({
+        id, 
+        name,
+      }));
+      setstudent(formattedResponse);  
+    } catch (error) {
+      console.error('Erro ao buscar os dados:', error);
+    }
+  }
 
   const load = async () => {
     toast.current?.show({ severity: 'info', summary: 'Info', detail: 'Carregando', life: 4000 });
@@ -41,39 +56,46 @@ const FrequencyTeacher = () => {
     }, 3000)
   }
 
-  useEffect(() => {
-    setstudent(studentList);
-  }, []);
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Loader isLoading={loading} />
+      </div>
+    )
+  }
 
   return (
-    <div className='flex flex-col items-center justify-center w-full'>
-      <div className='bg-orange-400 w-full h-auto flex flex-col p-4'>
-        <Menu />
-        <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Selecione a turma</h3>
-        <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Frequência</h3>
+    <div className={style.fade_in}>
+      <div className='flex flex-col items-center justify-center w-full'>
+        <div className='bg-orange-400 w-full h-auto flex flex-col p-4'>
+          <Menu />
+          <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>Frequência</h3>
+          <h3 className='text-center mt-0.5 text-amber-50 text-2xl'>{nameGrade}</h3>
+        </div>
+        <div className='pt-1 grid grid-cols-2 gap-7 justify-center w-full max-w-lg'>
+          <h2 className='flex-1 text-center font-bold text-black text-xs sm:text-base mt-5'>Presenças</h2>
+          <h2 className='flex-1 text-center font-bold text-black text-xs sm:text-base mt-5'>Data: 03/02/2025</h2>
+        </div>
+        <div className="mb-8"></div>
+        <Toast ref={toast} />
+        <div className="card w-full overflow-x-auto px-4 sm:px-8 md:px-80">
+          <DataTable
+            value={student}
+            selectionMode={rowClick ? 'multiple' : 'checkbox'}
+            selection={selectedstudent!}
+            onSelectionChange={(e: any) => setSelectedstudent(e.value)}
+            dataKey="id"
+            tableStyle={{ minWidth: '25rem' }}
+          >
+            <Column field="name" header="Aluno"></Column>
+            <Column selectionMode="multiple" headerStyle={{ width: '10rem' }} header="Selecionar todos os alunos"></Column>
+          </DataTable>
+        </div>
+        <ButtonGeneral styles={{ 'backgroundColor': 'orange' }} label='Matrícula' submit={undefined} click={load} />
+        <div className='mb-20'></div>
       </div>
-      <div className='pt-1 grid grid-cols-2 gap-7 justify-center w-full max-w-lg'>
-        <h2 className='flex-1 text-center font-bold text-black text-xs sm:text-base mt-5'>Presenças</h2>
-        <h2 className='flex-1 text-center font-bold text-black text-xs sm:text-base mt-5'>Data: 03/02/2025</h2>
-      </div>
-      <div className="mb-8"></div>
-      <Toast ref={toast} />
-      <div className="card w-full overflow-x-auto px-4 sm:px-8 md:px-80">
-        <DataTable
-          value={student}
-          selectionMode={rowClick ? 'multiple' : 'checkbox'}
-          selection={selectedstudent!}
-          onSelectionChange={(e: any) => setSelectedstudent(e.value)}
-          dataKey="id"
-          tableStyle={{ minWidth: '25rem' }}
-        >
-          <Column field="name" header="Aluno"></Column>
-          <Column selectionMode="multiple" headerStyle={{ width: '10rem' }} header="Selecionar todos os alunos"></Column>
-        </DataTable>
-      </div>
-      <ButtonGeneral styles={{ 'backgroundColor': 'orange' }} label='Matrícula' submit={undefined} click={load} />
-      <div className='mb-20'></div>
     </div>
+
   )
 }
 
