12501a85333efe61851acb28d73308433eb24d67
EliasBRodrigues <elias.rborges3@gmail.com>
2025-03-15 21:32:41 -0300

    feat: Object[] List return data from table Teachers

--- server/src/main/java/com/gestao_academica_backend/server/repository/TeacherRepository.java
+++ server/src/main/java/com/gestao_academica_backend/server/repository/TeacherRepository.java
@@ -9,8 +9,10 @@ import org.springframework.data.repository.query.Param;
 import com.gestao_academica_backend.server.models.Teachers;
 import com.gestao_academica_backend.server.models.dto.TeacherDTO;
 
-
 public interface TeacherRepository extends JpaRepository<Teachers, Long> {
     @Query("SELECT new com.gestao_academica_backend.server.models.dto.TeacherDTO(t.name, t.email) FROM Teachers t where t.email LIKE %:email%")
     TeacherDTO findTeacherByUserEmail(@Param("email") String email);
+
+    @Query(value = "SELECT t.email, t.name, s.name_section, g.name_grade, g.sigla FROM Teachers t join teacher_section ts on ts.teacher_id = t.id join sections s on s.id = ts.section_id  join grade g on g.id = s.grade_id  where t.email LIKE %:email%", nativeQuery = true)
+    List<Object[]> findSubjectsTeacher(@Param("email") String email);
 }
